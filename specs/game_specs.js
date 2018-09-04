1eaeb71ec13cecce31170226a4c80487fa0c22cc
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-04 15:24:21 +0100

    Game has player1, player2, deck created and tested

--- specs/game_specs.js
+++ specs/game_specs.js
@@ -11,8 +11,29 @@ let card2;
 
 beforeEach( function(){
   game = new Game(player1, player2);
-  player1 = new Player('Player1');
-  player2 = new Player('Player2');
+  player1 = new Player('John');
+  player2 = new Player('Chris');
   card1 = new Card('Superman', 6, 9, 7);
   card2 = new Card('Scarlet Witch', 7, 10, 5);
+});
+
+
+describe("Player", function(){
+  it("should have a player1", function(){
+    const actual = game.player1.name;
+    assert.strictEqual(actual, 'John');
+  });
+
+
+  it("should have a player2", function(){
+    const actual = game.player2.name;
+    assert.strictEqual(actual, 'Chris');
+  });
+
+  it("should have a deck", function(){
+    const actual = game.deck;
+    assert.deepStrictEqual(actual, []);
+  });
+
+
 });

6eba8c09e131616df3ccec5e6026f4e051a61b74
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-04 15:19:28 +0100

    Class  of Game created

--- game.js
+++ game.js
@@ -1,18 +1,15 @@
-const assert = require('assert');
-const Game = require('../game.js');
-const Player = require('../player.js');
-const Card = require('../card.js');
+const Game = function(player1, player2) {
+  this.player1 = player1;
+  this.player2 = player2;
+  this.deck = [];
+};
 
-let game;
-let player1;
-let player2;
-let card1;
-let card2;
 
-beforeEach( function(){
-  game = new Game(player1, player2);
-  player1 = new Player('Player1');
-  player2 = new Player('Player2');
-  card1 = new Card('Superman', 6, 9, 7);
-  card2 = new Card('Scarlet Witch', 7, 10, 5);
-});
+
+
+
+
+
+
+
+module.exports = Game;
